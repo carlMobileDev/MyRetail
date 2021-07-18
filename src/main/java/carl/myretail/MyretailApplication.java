cb6c7d78b95c2f538779eb5d6e3bbe78cc572782
Carl Wills <carl.r.wills@gmail.com>
2021-07-18 10:40:21 -0500

    Add basic spring-boot-starter caching on the red sky api requests

--- src/main/java/carl/myretail/MyretailApplication.java
+++ src/main/java/carl/myretail/MyretailApplication.java
@@ -2,10 +2,11 @@ package carl.myretail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableCaching
 @EnableMongoRepositories(basePackages = "carl.myretail.repository")
 public class MyretailApplication {
 
@@ -13,5 +14,5 @@ public class MyretailApplication {
 		SpringApplication.run(MyretailApplication.class, args);
 		
 	}
-	
+
 }
--- src/main/java/carl/myretail/service/redSky/RedSkyService.java
+++ src/main/java/carl/myretail/service/redSky/RedSkyService.java
@@ -4,6 +4,7 @@ package carl.myretail.service.redSky;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.cache.annotation.Cacheable;
 import org.json.JSONObject; 
 
 import io.github.resilience4j.bulkhead.annotation.Bulkhead;
@@ -14,6 +15,7 @@ public class RedSkyService {
     
     @Bulkhead(name= "redSkyService")
     @CircuitBreaker(name="redSkyService")
+    @Cacheable("products")
     public String getNameForProductId(long id) {
 
         String url = "https://redsky.target.com/v3/pdp/tcin/{id}?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics&key=candidate";
